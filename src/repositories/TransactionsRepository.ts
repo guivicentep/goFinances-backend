206657893c3251b4747eb7f8a2cca803e77bb672
guivicentep <guivicente.gvp@hotmail.com>
2020-06-28 18:54:53 -0300

    Commit pós alterações

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface Balance {
   income: number;
@@ -8,10 +9,40 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  id: string;
+  title: string;
+  value: number;
+  type: string;
+  category: Category;
+  created_at: Date;
+  updated_at: Date;
+}
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    const transactionList = this.find();
+
+    (await transactionList).map(transaction => {
+      if (transaction.type === 'income') {
+        balance.income += transaction.value;
+      }
+      if (transaction.type === 'outcome') {
+        balance.outcome += transaction.value;
+      }
+
+      return balance;
+    });
+    balance.total = +balance.income - balance.outcome;
+
+    return balance;
   }
 }
 
